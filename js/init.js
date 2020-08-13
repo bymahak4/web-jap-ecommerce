2cb3496958f44d7aae7cba738b3ef0476616a084
byMahaka <69089098+byMahaka@users.noreply.github.com>
2020-08-12 21:29:35 -0300

    Actualizacion 1
    + Modifico Product.html, Cambio nombre de Index.html a Home.html, Cambio Login.html por Index.html, modifico el index.html como login.

--- js/init.js
+++ js/init.js
@@ -7,6 +7,7 @@ const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/pro
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 
+// EFECTO
 var showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
 }
--- js/products.js
+++ js/products.js
@@ -1,6 +1,45 @@
+var categoriesArray = [];
+
+function showCategoriesList(array){
+
+    let htmlContentToAppend = "";
+    for(let i = 0; i < array.length; i++){
+        let category = array[i];
+
+        htmlContentToAppend += `
+        <div class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col-3">
+                    <img src="` + category.imgSrc + `" alt="` + category.description + `" class="img-thumbnail">
+                </div>
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ category.name +`</h4>
+                        <small class="text-muted">` + category.soldCount + ` artículos</small>
+                    </div>
+                    <div>
+                        <p>`+ category.description +`</p>
+                        <p><b>Precio:</b> US$ ` + category.cost +`</p>
+                    </div>     
+                </div>
+            </div>
+        </div>
+        `
+
+        document.getElementById("cat-list-container").innerHTML = htmlContentToAppend;
+    }
+}
+
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function (e) {
-
+document.addEventListener("DOMContentLoaded", function(e){ 
+    getJSONData(PRODUCTS_URL).then(function(resultObj){
+        if (resultObj.status === "ok")
+        {
+            categoriesArray = resultObj.data;
+            //Muestro las categorías ordenadas
+            showCategoriesList(categoriesArray);
+        }  
+    });
 });
