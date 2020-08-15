fedb7f06f6a0e9aa2d13aefe3068814f0202b24a
byMahaka <69089098+byMahaka@users.noreply.github.com>
2020-08-14 23:04:58 -0300

    Actualizacion 2.1
    + Borrado de login.js

--- js/login.js
+++ js/login.js
@@ -2,14 +2,6 @@
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
 
-function onSingIn(googleUser) {
-    var profile = googleUser.getBasicProfile();
-    var idToken = googleUser.getAuhResponse().idToken;
-    if (idToken != undefined || idToken != null){
-        window.location.href = "Home.html"
-    };
-}
-
 document.addEventListener("DOMContentLoaded", function(e){
     
 });
