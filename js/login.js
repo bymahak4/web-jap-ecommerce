a069b7afa180af56f8e2865c7a2adfeb15023330
byMahaka <69089098+byMahaka@users.noreply.github.com>
2020-08-15 03:28:22 -0300

    Actualizacion 2.3
    +Log Google

--- js/login.js
+++ js/login.js
@@ -1,6 +1,8 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+function onSignIn(googleUser){
+    Console.log("Usuario google")}
 document.addEventListener("DOMContentLoaded", function(e){
     
 });
