65bf80dfb10dfbaed2d778b9fcd4aa2a4cdc7eb4
byMahaka <69089098+byMahaka@users.noreply.github.com>
2020-08-14 21:48:15 -0300

    Actualizacion 2
    + Se modifico product.html arreglando la cantidad de autos vendidos y resaltandolo.
    + Se agrego Login con Google.

--- js/login.js
+++ js/login.js
@@ -1,6 +1,15 @@
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
+
+function onSingIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    var idToken = googleUser.getAuhResponse().idToken;
+    if (idToken != undefined || idToken != null){
+        window.location.href = "Home.html"
+    };
+}
+
 document.addEventListener("DOMContentLoaded", function(e){
     
 });
--- js/products.js
+++ js/products.js
@@ -15,7 +15,7 @@ function showCategoriesList(array){
                 <div class="col">
                     <div class="d-flex w-100 justify-content-between">
                         <h4 class="mb-1">`+ category.name +`</h4>
-                        <small class="text-muted">` + category.soldCount + ` artículos</small>
+                        <small class="text-muted"><b>` + category.soldCount + ` Vendididos</b></small>
                     </div>
                     <div>
                         <p>`+ category.description +`</p>
