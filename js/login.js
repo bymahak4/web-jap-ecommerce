1d93262ff75b4042303a7d7cdbbf278988a7687b
byMahaka <69089098+byMahaka@users.noreply.github.com>
2020-08-30 22:17:15 -0300

    ModLog

--- js/login.js
+++ js/login.js
@@ -2,16 +2,13 @@ var formulario = document.forms[0];
 
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    
+ 
     localStorage.setItem('Name',profile.getName());
 
-    if (localStorage.getItem('Name') != undefined || sessionStorage.getItem('Name') != null){
-        
-        localStorage.setItem('Name',profile.getGivenName());
-        localStorage.setItem('Email',profile.getEmail());
-    
-        window.location.href = "home.html";        
-  }
+    localStorage.setItem('Name',profile.getGivenName());
+     
+    window.location.href = "home.html"; 
+          
 }
 
     formulario.addEventListener('submit', function(e){
